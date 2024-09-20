a98f250ba77f9730a8390875c8df612a40723941
Lina <londonohidalgo@gmail.com>
2024-09-20 14:39:25 +0200

    deploi

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('https://whatyearisit-backend-three-gamma.vercel.app/')
+fetch('https://whatyearisit-backend-three-gamma.vercel.app/users')
      .then(response => response.json())
      .then(data => {
         document.querySelector('#year').textContent = data; 
