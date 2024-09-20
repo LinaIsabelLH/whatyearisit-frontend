95c203cad9c99265f3ca39c3609c043d5d79874d
Lina <londonohidalgo@gmail.com>
2024-09-20 14:43:14 +0200

    aa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-fetch('https://whatyearisit-backend-three-gamma.vercel.app/users')
+fetch('https://whatyearisit-backend-three-gamma.vercel.app/years')
      .then(response => response.json())
      .then(data => {
-        document.querySelector('#year').textContent = data; 
+        document.querySelector('#year').textContent = data.years; 
      });
